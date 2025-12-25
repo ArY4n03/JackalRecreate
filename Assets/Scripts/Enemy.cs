dcfb1b38aa53fdbb2fba68b71d0b5ec9fc5a88f2
Aryan Soy <aryansoy3@gmail.com>
2025-12-25 16:02:02 +0530

    added enemy animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,69 +3,48 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private float bullet_speed = 2.2f;
-    private float cooldown = 1.5f;
-    private float lastShootTime;
-    private Rigidbody2D rb;
-    private PlayerAwarenessController awarenessController;
-    private Vector2 targetDir;
-    [SerializeField] private GameObject bulletPrefab;
 
+    private PlayerAwarenessController awarenessController;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sr;
+    private float speed = 3.5f;
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
         awarenessController = GetComponent<PlayerAwarenessController>();
-
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+        sr = GetComponentInChildren<SpriteRenderer>();
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+
+    private void Update()
+    {
+        handleAnimation();
+    }
+    private void FixedUpdate()
+    {
+        moveTowardsPlayer();
+    }
+    private void moveTowardsPlayer()
     {
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        updateDirection();
-
-        float timeSinceLastShoot = Time.time - lastShootTime;
-
-        if (timeSinceLastShoot >= cooldown)
-        {
-            shootPlayer();
-            lastShootTime = Time.time;
-        }
-        
-        
-    }
-
-    private void updateDirection()
-    {
-        if(awarenessController.isAware)
-        {
-            targetDir = awarenessController.PlayerDir;
-        }
-        else
-        {
-            targetDir = Vector2.zero;
-        }
-
-    }
-
-    private void shootPlayer()
-    {
         if (awarenessController.isAware)
         {
-            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
-            Rigidbody2D bullet_rb = bullet.GetComponent<Rigidbody2D>();
-            bullet.GetComponent<Bullet>().isPlayerBullet = false;
-            bullet_rb.linearVelocity = bullet_speed * targetDir;
+            rb.linearVelocityX = awarenessController.PlayerDir.x * speed;
+
+            bool flip = rb.linearVelocityX < 0;
+            sr.flipX = flip;
+
+            
         }
     }
 
+
+    private void handleAnimation() => anim.SetBool("isAware", awarenessController.isAware);
+  
     public void OnHit()
     {
         Destroy(gameObject);
-        Debug.Log("dead");
     }
 }
