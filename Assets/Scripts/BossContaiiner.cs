96ff17895fbb609cecf2a6611457e865883456df
Aryan Soy <aryansoy3@gmail.com>
2025-12-31 13:37:41 +0530

    smooth level transitions

--- Assets/Scripts/BossContaiiner.cs
+++ Assets/Scripts/BossContaiiner.cs
@@ -3,11 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class BossContaiiner : MonoBehaviour
 {
+    private GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = FindAnyObjectByType<GameManager>();
+    }
     void Update()
     {
         if(transform.childCount == 0)
         {
-            SceneManager.LoadScene("SampleScene");
+            gameManager.load_nextScene();
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
     public int score=0;
     public int playerLife;
+    [SerializeField] private string next_level;
 
     public void increment_score(int value)
     {
         score += value;
+    }
+
+    public void load_nextScene()
+    {
+        SceneManager.LoadScene(next_level);
     }
 }
